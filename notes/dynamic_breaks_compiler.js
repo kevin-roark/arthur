ad2ff7330a01fdf9719d3298961549abd4f3d815
Kevin Roark <ker2143@columbia.edu>
2014-02-17 14:50:41 -0500

    initial notes

---
+++ notes/dynamic_breaks_compiler.js
@@ -0,0 +1,19 @@
+
+/** 
+ * This would make our language explode ... would never compile
+ *
+ * Need to think of a solution
+ */
+
+while (1) {
+    Sound x = x * z;
+    arthur.add(x);
+}
+
+/*
+
+We are using java to do the audio processing. And then adding the complete
+audio files to the end javacsript. But if we are doing processing in an
+infinite loop then we never make it to the javascript.
+
+*/
