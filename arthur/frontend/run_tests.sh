e3ace573fe47ecb7c6faafcef274b84c88567776
henry van dusen <hgv2102@columbia.edu>
2014-04-02 17:46:34 -0400

    hey

--- arthur/frontend/run_tests.sh
+++ arthur/frontend/run_tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+c=0
 # gotta go up some levels..
 cd ../../
 
@@ -9,3 +9,7 @@ echo $CLASSPATH
 java arthur/frontend/yacctest samples/sample1.art
 java arthur/frontend/yacctest samples/sample2.art
 java arthur/frontend/yacctest samples/sample3.art
+java arthur/frontend/yacctest samples/sample4.art
+
+
+
