82b6e1f0240af940bf23d7fdead43dec2cd58dd0
Kevin Roark <ker2143@columbia.edu>
2014-04-02 23:21:21 -0400

    cleaner

--- supercleaner.sh
+++ supercleaner.sh
@@ -3,11 +3,12 @@
 rm -f arthur/frontend/*.class;
 rm -f arthur/frontend/Lexer.java~
 
+rm -f arthur/backend/*.class;
+rm -f arthur/backend/builtins/java/*.class;
 rm -f arthur/backend/translator/*.class;
+rm -f arthur/backend/media/*.class;
+rm -f arthur/backend/whisperer/*.class;
 rm -f arthur/backend/translator/java/*.class;
 rm -f arthur/backend/translator/js/*.class;
 
-rm -rf arthur/*.class;
 rm -rf ./*.class
-
-rm -f ArthurTranslation.java
