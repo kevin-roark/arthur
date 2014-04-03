90c815f5a40a27c5e1f67d6e8c4bf76524ec9049
Kevin Roark <ker2143@columbia.edu>
2014-04-03 02:52:19 -0400

    added some basic client stuff

---
+++ arthur/client/js/arthur/index.js
@@ -0,0 +1,31 @@
+
+module.exports.ArthurColor = require('./arthur-color');
+module.exports.ArthurNumber = require('./arthur-number');
+module.exports.ArthurString = require('./arthur-string');
+module.exports.ArthurImage = require('./arthur-image');
+module.exports.ArthurSound = require('./arthur-sound');
+module.exports.ArthurVideo = require('./arthur-video');
+
+module.exports.addArthurColor = function(filename, frame) {
+
+}
+
+module.exports.addArthurNumber = function(filename, frame) {
+
+}
+
+module.exports.addArthurString = function(filename, frame) {
+
+}
+
+module.exports.addArthurImage = function(filename, frame) {
+
+}
+
+module.exports.addArthurSound = function(filename, frame) {
+
+}
+
+module.exports.addArthurVideo = function(filename, frame) {
+
+}
