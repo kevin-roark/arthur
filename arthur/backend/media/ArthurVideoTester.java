06aa694eddf6c753afd92195e6194f4877af19d6
Kevin Roark <ker2143@columbia.edu>
2014-04-23 14:17:31 -0400

    russian video workin' in video tests

--- arthur/backend/media/ArthurVideoTester.java
+++ arthur/backend/media/ArthurVideoTester.java
@@ -5,9 +5,11 @@ public class ArthurVideoTester {
   public static void main(String[] args) {
     String f1 = "ball.mp4";
     String f2 = "ZERO.mp4";
+    String f3 = "russian.mp4";
 
     ArthurVideo v1 = new ArthurVideo(f1);
     ArthurVideo v2 = new ArthurVideo(f2);
+    ArthurVideo v03 = new ArthurVideo(f3);
 
     //ArthurVideo v3 = v1.add(v2);
 
@@ -31,10 +33,18 @@ public class ArthurVideoTester {
                         .add(new ArthurSound("future.mp3"))
                         ;*/
 
-    ArthurVideo v12 = v1.multiply(v2);
-    ArthurVideo v13 = v1.divide(v2);
+    //ArthurVideo v12 = v1.multiply(v2);
+    //ArthurVideo v13 = v1.divide(v2);
 
-    System.out.println("v12: " + v12.filename);
-    System.out.println("v13: " + v13.filename);
+    //System.out.println("v12: " + v12.filename);
+    //System.out.println("v13: " + v13.filename);
+
+    ArthurVideo v14 = v2.multiply(v03);
+    ArthurVideo v15 = v03.multiply(v14);
+    ArthurVideo v16 = v2.divide(v03);
+
+    System.out.println("v14: " + v14.filename);
+    System.out.println("v15: " + v15.filename);
+    System.out.println("v16: " + v16.filename);
   }
 }
--- arthur/backend/media/vidoverlay.sh
+++ arthur/backend/media/vidoverlay.sh
@@ -5,7 +5,7 @@
 ##scale it by the size of the first vid
 ffmpeg -i $2 -vf "scale=$4, setsar=1/1" -strict -2 output.avi
 
-ffmpeg -i $1 -vf "setsar=1/1" -strict -2 onesar.mp4
+ffmpeg -i $1 -vf "scale=$4, setsar=1/1" -strict -2 onesar.mp4
 
 ffmpeg -i onesar.mp4 -i output.avi -strict -2 -filter_complex "blend=all_mode='overlay':all_opacity=0.7" $3
 
